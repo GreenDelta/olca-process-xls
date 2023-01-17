bcacd810a3256614b84e94d2b9a9fcaa97b62ffe
msrocka <michael.srocka@gmail.com>
2023-01-17 18:43:23 +0100

    prepare location test

--- src/test/java/org/openlca/io/xls/process/BaseTest.java
+++ src/test/java/org/openlca/io/xls/process/BaseTest.java
@@ -9,46 +9,68 @@ import org.openlca.core.database.IDatabase;
 import org.openlca.core.model.Category;
 import org.openlca.core.model.Flow;
 import org.openlca.core.model.FlowProperty;
+import org.openlca.core.model.Location;
 import org.openlca.core.model.ModelType;
 import org.openlca.core.model.Process;
+import org.openlca.core.model.ProcessDocumentation;
 import org.openlca.core.model.UnitGroup;
 import org.openlca.core.model.store.InMemoryStore;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class BaseTest {
 
 	private final IDatabase db = Tests.db();
-	private final InMemoryStore store = InMemoryStore.create();
-	private Process process;
+	private Process origin;
+	private Process synced;
 
 	@Before
-	public void setup() {
+	public void setup() throws IOException {
 		var units = UnitGroup.of("Mass units", "kg");
 		var mass = FlowProperty.of("Mass", units);
 		var p = Flow.product("p", mass);
-		process = Process.of("P", p);
+
+		origin = Process.of("P", p);
+		origin.documentation = new ProcessDocumentation();
 		var root = Category.of("some", ModelType.PROCESS);
-		process.category = Category.childOf(root, "category");
-		store.insert(units, mass, p, process);
+		origin.category = Category.childOf(root, "category");
+
+		var location = Location.of("Aruba", "AW");
+		origin.location = location;
+		origin.documentation.geography = "about geography";
+
+		// write and read
+		var store = InMemoryStore.create();
+		store.insert(units, mass, p, location, origin);
+		var file = Files.createTempFile("_olca_", ".xlsx");
+		XlsProcessWriter.of(store)
+			.write(origin, file.toFile());
+		synced = XlsProcessReader.of(db)
+			.sync(file.toFile())
+			.orElseThrow();
+		Files.delete(file);
 	}
 
 	@After
 	public void cleanup() {
 		db.clear();
-		store.clear();
 	}
 
 	@Test
 	public void testBasicMetaData() {
-		var file = new File("target/process.xlsx");
-		XlsProcessWriter.of(store).write(process, file);
-		var copy = XlsProcessReader.of(db)
-			.sync(file)
-			.orElseThrow();
-		assertEquals(process.refId, copy.refId);
-		assertEquals(process.name, copy.name);
-		assertEquals(process.lastChange, copy.lastChange);
-		assertEquals(process.category.toPath(), "some/category");
+		assertEquals(origin.refId, synced.refId);
+		assertEquals(origin.name, synced.name);
+		assertEquals(origin.lastChange, synced.lastChange);
+		assertEquals(origin.category.toPath(), "some/category");
+	}
+
+	@Test
+	public void testGeography() {
+		var loc = synced.location;
+		assertNotNull(loc);
+		assertEquals("Aruba", loc.name);
+		assertEquals("AW", loc.code);
+		assertEquals("about geography", synced.documentation.geography);
 	}
 }
